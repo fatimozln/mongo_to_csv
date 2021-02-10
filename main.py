00a0b4d12298288b8945066cd06e3d61658531e5
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-02-10 16:52:09 +0330

    update for add to csv file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pymongo
+import csv
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
@@ -9,3 +10,12 @@ information = mydb.documents
 for record in information.find():
     print('firstname is', record['firstname'], 'and lastname is',
           record['lastname'], 'and age =', record['age'])
+
+
+with open('names.csv', 'w', newline='') as csvfile:
+    fieldnames = ['firstname', 'lastname', 'age']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for record in information.find():
+        writer.writerow(
+            {'firstname': record['firstname'], 'lastname': record['lastname'], 'age': record['age']})
